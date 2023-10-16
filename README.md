6782f0a37b27a8f39decf2f74564989bfacc2bd2
JustNU <ivancool108@gmail.com>
2023-10-16 11:03:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fix Defaul tDigital Flora
+Retextures some of the digital flora gear to a more correct variants
---
+++ src/mod.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+class Mod
+{
+	postDBLoad(container) 
+	{
+		// Constants
+		const logger = container.resolve("WinstonLogger");
+		const database = container.resolve("DatabaseServer").getTables();
+		const config = require("../config/config.json");
+		
+		if (config.Change_6B13)
+			database.templates.items["5c0e53c886f7747fa54205c7"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6b13.bundles";
+	}
+}
+
+module.exports = { mod: new Mod() }
