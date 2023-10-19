21558a00d86b13c55016de1b3bb3f1c75a939a7e
JustNU <ivancool108@gmail.com>
2023-10-19 10:55:32 +0300

    add 6b43 variants, improve 6b23

--- src/mod.js
+++ src/mod.js
@@ -16,10 +16,28 @@ class Mod
 		if (config.Vanilla["Change 6B23-1"])
 			database.templates.items["5c0e5bab86f77461f55ed1f3"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6b23.bundle";
 		
+		if (config.Vanilla["Change 6B43 Full"])
+			database.templates.items["545cdb794bdc2d3a198b456a"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6b43_full.bundle";
+		
 		// AdditionalGear - Vanilla Extended
 		if (config["AdditionalGear - Vanilla Extended"]["Change 6B23-2"]) {
 			if (database.templates.items["AddGearVanExt_6B23_2_Digital_Flora"])
 				database.templates.items["AddGearVanExt_6B23_2_Digital_Flora"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6b23.bundle";
+		}
+		
+		if (config["AdditionalGear - Vanilla Extended"]["Change 6B43 Assault"]) {
+			if (database.templates.items["AddGearVanExt_6B43_Assault"])
+				database.templates.items["AddGearVanExt_6B43_Assault"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6B43_assault.bundle";
+		}
+		
+		if (config["AdditionalGear - Vanilla Extended"]["Change 6B43 Mobility"]) {
+			if (database.templates.items["AddGearVanExt_6B43_Mobility"])
+				database.templates.items["AddGearVanExt_6B43_Mobility"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6B43_mobility.bundle";
+		}
+		
+		if (config["AdditionalGear - Vanilla Extended"]["Change 6B43 Light"]) {
+			if (database.templates.items["AddGearVanExt_6B43_Light"])
+				database.templates.items["AddGearVanExt_6B43_Light"]._props.Prefab.path = "FixDefaultDigitalFlora/ArmoredVests/6B43_light.bundle";
 		}
 	}
 }
